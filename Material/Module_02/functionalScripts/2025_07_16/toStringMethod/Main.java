a16fc70d6523b59e59c1fd3bd397843b979e692d
rjaidsvcet <raunak.joshi@vcet.edu.in>
2025-07-16 16:04:03 +0530

    toString Method

---
+++ Material/Module_02/functionalScripts/2025_07_16/toStringMethod/Main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        Viltrumites first = new Viltrumites("Nolan", "Omniman");
+        Viltrumites second = new Viltrumites("Mark", "Invincible");
+
+        System.out.println(first);
+        System.out.println(second);
+    }
+}
---
+++ Material/Module_02/functionalScripts/2025_07_16/toStringMethod/Viltrumites.java
@@ -0,0 +1,13 @@
+class Viltrumites {
+    String name;
+    String earthName;
+    
+    Viltrumites (String name, String earthName) {
+        this.name = name;
+        this.earthName = earthName;
+    }
+
+    public String toString () {
+        return this.name + "\n" + this.earthName;
+    }
+}
