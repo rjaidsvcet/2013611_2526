748cd4eb619b3e65f8fc55ac2000c0b3618a91ce
rjaidsvcet <raunak.joshi@vcet.edu.in>
2025-07-30 10:22:25 +0530

    Generic Java Servlet

---
+++ Material/Module_03/functionalScripts/2025_07_30/trialServlets/src/main/java/com/trial/AddServlet.java
@@ -0,0 +1,20 @@
+package com.trial;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+	public void doPost (HttpServletRequest req, HttpServletResponse res) throws IOException {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		int k = i + j;
+		
+		System.out.println("Result : "+k);
+		PrintWriter out = res.getWriter();
+		out.println ("Result:"+k);
+	}
+}
