30f3a459eacb908ff96481a7daec7eb3c3b1e484
rjaidsvcet <raunak.joshi@vcet.edu.in>
2025-08-13 11:11:40 +0530

    JavaScript intro

---
+++ Material/Module_04/functionalScripts/2025_08_13/main.js
@@ -0,0 +1,22 @@
+// const is immutable
+const name = 'Batman'
+// console.log(name);
+
+// let is mutable
+let newName = 'Superman'
+newName = 'Batman'
+// console.log(newName);
+
+// console.log(typeof (newName));
+
+const firstNumber = 1
+const secondNumber = "1"
+
+console.log(typeof (firstNumber));
+console.log(typeof (secondNumber));
+console.log(firstNumber === secondNumber);
+
+const myName = 'IronMan'
+// console.log("I am "+myName);
+// console.log (`I am ${myName}`)
+
