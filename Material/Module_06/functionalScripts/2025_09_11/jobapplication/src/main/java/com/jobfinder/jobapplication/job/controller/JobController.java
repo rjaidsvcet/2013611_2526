82d40ed621858aa6c159d0ce6243f42f992b0fe6
rjaidsvcet <raunak.joshi@vcet.edu.in>
2025-09-11 12:21:45 +0530

    Controller for Single ID GET

--- Material/Module_06/functionalScripts/2025_09_11/jobapplication/src/main/java/com/jobfinder/jobapplication/job/controller/JobController.java
+++ Material/Module_06/functionalScripts/2025_09_11/jobapplication/src/main/java/com/jobfinder/jobapplication/job/controller/JobController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +32,20 @@ public class JobController {
     public String addJob (@RequestBody Job job) {
         service.createJob(job);
         return "Works";
+    }
+
+    @GetMapping("/jobs/{id}")
+    public Job getSingleJob(@PathVariable Long id) {
+        Job job = service.getJobById(id);
+        if (job != null)
+            return job;
+        return new Job(
+            1L,
+            "testTitle",
+            "testDesc",
+            "test minSal",
+            "test maxSal",
+            "testLocation"
+        );
     }
 }
