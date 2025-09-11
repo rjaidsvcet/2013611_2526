52f6ae3c92fc22c6a7c71dc098e448bf2a74b68e
rjaidsvcet <raunak.joshi@vcet.edu.in>
2025-09-11 12:07:39 +0530

    Controller with service Auto-wired

--- Material/Module_06/functionalScripts/2025_09_11/jobapplication/src/main/java/com/jobfinder/jobapplication/job/controller/JobController.java
+++ Material/Module_06/functionalScripts/2025_09_11/jobapplication/src/main/java/com/jobfinder/jobapplication/job/controller/JobController.java
@@ -1,29 +1,35 @@
 package com.jobfinder.jobapplication.job.controller;
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jobfinder.jobapplication.job.Job;
+import com.jobfinder.jobapplication.job.service.JobService;
 
 @RestController
 public class JobController {
-    private List<Job> jobs = new ArrayList<>();
-    private Long nextId = 1L;
+    @Autowired
+    private JobService service;
+
+    // Constructor for Service 
+    // public JobController(JobService service) {
+    //     this.service = service;
+    // }
 
     @GetMapping("/jobs")
     public List<Job> findAll () {
-        return jobs;
+        return service.findAll();
     }
 
     @PostMapping("/jobs")
     public String addJob (@RequestBody Job job) {
-        job.setId(nextId++);
-        jobs.add(job);
+        service.createJob(job);
         return "Works";
     }
 }
