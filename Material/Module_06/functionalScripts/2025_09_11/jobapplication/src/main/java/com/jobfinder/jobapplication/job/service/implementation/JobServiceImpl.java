ffb5be0c07c34f672606fc6a7356e5271ada2529
rjaidsvcet <raunak.joshi@vcet.edu.in>
2025-09-11 12:07:20 +0530

    Implementation of the Service Methods

---
+++ Material/Module_06/functionalScripts/2025_09_11/jobapplication/src/main/java/com/jobfinder/jobapplication/job/service/implementation/JobServiceImpl.java
@@ -0,0 +1,28 @@
+package com.jobfinder.jobapplication.job.service.implementation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.jobfinder.jobapplication.job.Job;
+import com.jobfinder.jobapplication.job.service.JobService;
+
+@Service
+public class JobServiceImpl implements JobService {
+
+    private List<Job> jobs = new ArrayList<>();
+    private Long newId = 1L;
+
+    @Override
+    public List<Job> findAll() {
+        return jobs;
+    }
+
+    @Override
+    public void createJob(Job job) {
+        job.setId(newId++);
+        jobs.add(job);
+    }
+    
+}
