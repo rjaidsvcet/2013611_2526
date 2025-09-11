6037fde8493be6e5c1456ce889b514c92ed0355a
rjaidsvcet <raunak.joshi@vcet.edu.in>
2025-09-11 11:42:01 +0530

    Post Method

--- Material/Module_06/functionalScripts/2025_09_11/jobapplication/src/main/java/com/jobfinder/jobapplication/job/controller/JobController.java
+++ Material/Module_06/functionalScripts/2025_09_11/jobapplication/src/main/java/com/jobfinder/jobapplication/job/controller/JobController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jobfinder.jobapplication.job.Job;
@@ -11,9 +13,17 @@ import com.jobfinder.jobapplication.job.Job;
 @RestController
 public class JobController {
     private List<Job> jobs = new ArrayList<>();
+    private Long nextId = 1L;
 
     @GetMapping("/jobs")
     public List<Job> findAll () {
         return jobs;
+    }
+
+    @PostMapping("/jobs")
+    public String addJob (@RequestBody Job job) {
+        job.setId(nextId++);
+        jobs.add(job);
+        return "Works";
     }
 }
