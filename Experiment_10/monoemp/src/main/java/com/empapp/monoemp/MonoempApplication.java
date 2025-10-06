aa76767fcb9f9a81d9dc9c17f30b60ee9792d1c8
rjaidsvcet <raunak.joshi@vcet.edu.in>
2025-10-06 14:32:10 +0530

    Base Files

---
+++ Experiment_10/monoemp/src/main/java/com/empapp/monoemp/MonoempApplication.java
@@ -0,0 +1,13 @@
+package com.empapp.monoemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonoempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonoempApplication.class, args);
+	}
+
+}
