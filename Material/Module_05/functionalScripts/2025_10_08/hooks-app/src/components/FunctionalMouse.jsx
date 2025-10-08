ad5a6ec8e29ce8b1d8834193b1551df821929897
rjaidsvcet <raunak.joshi@vcet.edu.in>
2025-10-08 09:01:48 +0530

    Execute only once

---
+++ Material/Module_05/functionalScripts/2025_10_08/hooks-app/src/components/FunctionalMouse.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react"
+
+function FunctionalMouse() {
+    const [x, setX] = useState (0)
+    const [y, setY] = useState (0)
+
+    const logMousePosition = e => {
+        console.log('Mouse Specific Event')
+        setX (e.clientX)
+        setY (e.clientY)
+    }
+
+    useEffect (() => {
+        console.log('useEffect Called')
+        window.addEventListener('mousemove', logMousePosition)
+    }, [])
+
+    return (
+        <div>
+            <h1>
+                X - {x}
+                Y - {y}
+            </h1>
+        </div>
+    )
+}
+
+export default FunctionalMouse
